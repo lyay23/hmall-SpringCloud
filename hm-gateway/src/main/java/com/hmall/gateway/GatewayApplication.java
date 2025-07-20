c97fb599115c0b2674799473a7f9208ade15ce62
lyay23 <3050681193@qq.com>
2025-07-20 16:23:24 +0800

    添加网关

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 李阳
+ * @Date: 2025/07/20/9:29
+ * @Description:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
