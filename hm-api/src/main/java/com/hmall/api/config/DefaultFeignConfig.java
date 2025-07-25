f399cf5069cedcb9acd03869cd5540944ed2199d
lyay23 <3050681193@qq.com>
2025-07-25 10:20:33 +0800

    OpenFeign传递用户到下游微服务

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -7,12 +7,39 @@ package com.hmall.api.config;
  * @Date: 2025/07/19/16:31
  * @Description:
  */
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLogLevel(){
         return Logger.Level.FULL;
+    }
+
+    /**
+     * 将用户信息添加请求头
+     * @return
+     */
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor(){
+        return new RequestInterceptor(){
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+
+                /**
+                 * 微服务调用微服务的请求是从网关传过来的，
+                 * 所以这时请求头中是有用户信息的，
+                 * 只需要从请求头中获取到用户信息，添加到请求头中即可
+                 * 如何请求再次传递给下一个微服务，这样就将用户信息传递给了其他微服务了
+                 */
+                Long userId = UserContext.getUser();
+                if (userId != null){
+                    requestTemplate.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 }
