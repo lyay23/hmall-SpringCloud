87652f52410e095a6b467768dda34b2b3f059ee5
lyay23 <3050681193@qq.com>
2025-07-30 14:14:07 +0800

    业务幂等性（业务判断方式）提示订单状态的健壮性

--- trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
+++ trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
@@ -1,5 +1,6 @@
 package com.hmall.trade.listener;
 
+import com.hmall.trade.domain.po.Order;
 import com.hmall.trade.service.IOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.ExchangeTypes;
@@ -32,6 +33,17 @@ public class PayStatusListener {
      * orderId: 订单id由发送方传入，哪一个订单支付成功了，就传入哪个订单id
      */
     public void listenPaySuccess(Long orderId) {
+
+        // 1. 查询订单
+        Order byId = orderService.getById(orderId);
+        // 2. 判断订单状态，是否为未支付
+       if(byId==null || byId.getStatus() != 1){
+
+           // 2.1 如果不是未支付状态，直接返回
+           return;
+       }
+
+        // 3. 标记订单为已支付
         orderService.markOrderPaySuccess(orderId);
     }
 }
